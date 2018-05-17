826b45aa9551971e085383ba7961e059b048eaf3
Giorgio Sgarbi <giorgiosgarbi@dhcp-128-189-192-140.ubcsecure.wireless.ubc.ca>
2018-05-17 11:09:30 -0700

    more locally balanced

--- src/main/java/matchings/BipartiteMatchingSampler.java
+++ src/main/java/matchings/BipartiteMatchingSampler.java
@@ -2,7 +2,9 @@ package matchings;
 
 import java.util.List;
 
-import bayonet.distributions.Multinomial;
+//time 91s, 73s, 68s, 83s
+
+//import bayonet.distributions.Multinomial;
 import bayonet.distributions.Random;
 import bayonet.math.SpecialFunctions;
 import blang.core.LogScaleFactor;
@@ -10,7 +12,7 @@ import blang.distributions.Generators;
 import blang.mcmc.ConnectedFactor;
 import blang.mcmc.SampledVariable;
 import blang.mcmc.Sampler;
-import briefj.collections.UnorderedPair;
+//import briefj.collections.UnorderedPair;
 
 /**
  * Each time a Permutation is encountered in a Blang model, 
@@ -33,16 +35,18 @@ public class BipartiteMatchingSampler implements Sampler {
   public void execute(Random rand) {
 	  
 	  double normF, //normalizing factor for locally balanced
-	  qAfterGivenBefore, qBeforeGivenAfter, logQAfterGivenBefore, logQBeforeGivenAfter;
+	  qAfterGivenBefore, 
+	  qBeforeGivenAfter, 
+	  logQAfterGivenBefore, 
+	  logQBeforeGivenAfter;
 	  int numConnected;
 	  
 	  //get the current log density
 	  double logDensityBefore = logDensity();
-
-	  
+  
 	  // choose randomly one vertex in the first component and a free vertex in component 2
 	  int i = Generators.discreteUniform(rand, 0, matching.componentSize());
-	  int j = 0;  // initialize it
+	  int j = 0; 
 	  if (matching.free2().isEmpty())
 		  ;
 	  else {
@@ -53,48 +57,42 @@ public class BipartiteMatchingSampler implements Sampler {
 	  //if that vertex is not connected, decide to make a connection operation
 	  boolean connectOp = matching.free1().contains(i);
 	  
-	  // if we don't make a connection operation, disconnect 
-	  // and get q's if a connection was not made
-
-		  qAfterGivenBefore =
-				  Math.sqrt((1/matching.componentSize()));
+	  if (connectOp) {
+		  // get logQ's if a connection was made
+		  qAfterGivenBefore = Math.sqrt(1/(matching.componentSize() * matching.free2().size()));
 		  numConnected = matching.componentSize() - matching.free1().size();
-		  normF =
-				  Math.exp(SpecialFunctions.logBinomial(numConnected, 2)) + 
+		  normF = Math.exp(SpecialFunctions.logBinomial(numConnected, 2)) + 
 				  matching.free1().size();
 		  logQAfterGivenBefore = Math.log (qAfterGivenBefore/normF);
 		  
+		  qBeforeGivenAfter = Math.sqrt(1/ (matching.componentSize() * matching.free2().size()));
+		  numConnected = matching.componentSize() - matching.free1().size();
+		  normF = Math.exp(SpecialFunctions.logBinomial(numConnected, 2)) + 
+				  matching.free1().size();
+		  logQBeforeGivenAfter = Math.log(qBeforeGivenAfter/normF);
+
+		  // Propose a matching by creating and edge between i and j
+		  matching.getConnections().set(i,j);
+	  }
+	  
+	  else {
+		  // get q's if a connection was not made
+		  qAfterGivenBefore = Math.sqrt((1/matching.componentSize()));
+		  numConnected = matching.componentSize() - matching.free1().size();
+		  normF = Math.exp(SpecialFunctions.logBinomial(numConnected, 2)) + 
+				  matching.free1().size();
+		  logQAfterGivenBefore = Math.log (qAfterGivenBefore/normF);
 		  
+		  // if we don't make a connection operation, disconnect 
 		  matching.getConnections().set(i, BipartiteMatching.FREE);
 		  
 		  qBeforeGivenAfter = Math.sqrt(1/ (matching.componentSize() * matching.free2().size()));
 		  numConnected = matching.componentSize() - matching.free1().size();
-		  normF =
-				  Math.exp(SpecialFunctions.logBinomial(numConnected, 2)) + 
+		  normF = Math.exp(SpecialFunctions.logBinomial(numConnected, 2)) + 
 				  matching.free1().size();
 		  logQBeforeGivenAfter = Math.log(qBeforeGivenAfter/normF);
-	  
-	  // else, choose to connect to one of the free vertexes of component 2
-	  if (connectOp) {
-		  // q's if a connection was made
-		  qAfterGivenBefore =
-				  Math.sqrt(1/(matching.componentSize() * matching.free2().size()));
-		  numConnected = matching.componentSize() - matching.free1().size();
-		  normF =
-				  Math.exp(SpecialFunctions.logBinomial(numConnected, 2)) + 
-				  matching.free1().size();
-		  logQAfterGivenBefore = Math.log (qAfterGivenBefore/normF);
-		  
-		  qBeforeGivenAfter = Math.sqrt(1/ (matching.componentSize() * matching.free2().size()));
-		  numConnected = matching.componentSize() - matching.free1().size();
-		  normF =
-				  Math.exp(SpecialFunctions.logBinomial(numConnected, 2)) + 
-				  matching.free1().size();
-		  logQBeforeGivenAfter = Math.log(qBeforeGivenAfter/normF);
-
-		  matching.getConnections().set(i,j);
 	  }
-	  
+	  	  
 	  //get our new logDensity
 	  double logDensityAfter = logDensity();
 	  
@@ -105,7 +103,7 @@ public class BipartiteMatchingSampler implements Sampler {
 	  if (Generators.bernoulli(rand, acceptPr)){
 	  // move to next state
 		  ;
-	  }
+		  }
 	  //else, go back
 	  else {
 		  if (connectOp) {
@@ -116,25 +114,14 @@ public class BipartiteMatchingSampler implements Sampler {
 		  // reconnect
 		  else {
 			  matching.getConnections().set(i, j);
-			  	  
+			  }
 		  }
-		  		
-
-
-		  }
-	  
-	 
-    
-  }
+	  }
   
   private double logDensity() {
     double sum = 0.0;
     for (LogScaleFactor f : numericFactors)
       sum += f.logDensity();
     return sum;
+    }
   }
-  
-  private void normProb() {
-	  ;
-  }
-}
